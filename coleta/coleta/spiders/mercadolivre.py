c624df26a47e8f4c64d3d28ef7fa2ddbf9e07c0c
FELLIPE <fellipeguedes@icloud.com>
2024-06-01 23:42:21 -0300

    quase finalizando com scrapy

--- coleta/coleta/spiders/mercadolivre.py
+++ coleta/coleta/spiders/mercadolivre.py
@@ -12,5 +12,10 @@ class MercadolivreSpider(scrapy.Spider):
 
         for product in products :
            yield {
-            "brand": product.css("span.ui-search-item__brand-discoverability.ui-search-item__group__element::text").get()
+            "brand": product.css("span.ui-search-item__brand-discoverability.ui-search-item__group__element::text").get(),
+            "name": product.css("h2.ui-search-item__title::text").get(),
+            #"old_price": product.css("andes-money-amount.ui-search-price__partui-search-price__part--small.ui-search-price__original-value.andes-money-amount--previous.andes-money-amount--cents-superscript.andes-money-amount--compact::text").get(),
+            #"new_price_": product.css("andes-money-amount ui-search-price__part ui-search-price__part--medium andes-money-amount--cents-superscript::text").get(),
+            "reviews_rating_number": product.css("span.ui-search-reviews__rating-number::text").get(),
+            "reviews_amount": product.css("span.ui-search-reviews__amount::text").get()
            }
